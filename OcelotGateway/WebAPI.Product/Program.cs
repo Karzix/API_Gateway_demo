59546474487500ec16fa5bc8697f1b8853131560
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2024-06-24 14:36:15 +0700

    a

--- OcelotGateway/WebAPI.Product/Program.cs
+++ OcelotGateway/WebAPI.Product/Program.cs
@@ -17,13 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors(builder =>
-{
-    builder
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader();
-});
+
 app.UseAuthorization();
 
 app.MapControllers();
