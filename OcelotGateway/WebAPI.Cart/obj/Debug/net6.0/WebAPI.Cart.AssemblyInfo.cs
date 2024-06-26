4d40c10766dff635bf1e4f678890b65831ec8840
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2024-06-26 11:32:06 +0700

    a

--- OcelotGateway/WebAPI.Cart/obj/Debug/net6.0/WebAPI.Cart.AssemblyInfo.cs
+++ OcelotGateway/WebAPI.Cart/obj/Debug/net6.0/WebAPI.Cart.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI.Cart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b17a5ec627cbc2f06ba5c977114716d8999dbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785d0de7c84e078ac685b4397a7896a61748dd4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI.Cart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI.Cart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
